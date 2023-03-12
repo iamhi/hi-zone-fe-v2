abbe171bb3990b4b0cf763336e2e1f27b18d561e
iamhi <inpactt@gmail.com>
2023-03-12 21:14:54 +0100

    Navigation menu with desktop support

--- src/components/NavigationComponent/NavigationComponent.jsx
+++ src/components/NavigationComponent/NavigationComponent.jsx
@@ -20,26 +20,32 @@ const NavigationComponent = () => {
 	};
 
 	return (
-		<div className="navigation-component">
-			<div className="navigation-component__current-path">
-				{pathname}
-			</div>
-			<ButtonComponent
-				onClick={showMenuAction}
-				customCss={classNames('navigation-component__toggle-button', {
-					'navigation-component__toggle-button--active': showList,
-				})}>
-				<i className="fa-solid fa-bars"></i>
-			</ButtonComponent>
+		<>
+			<div className="navigation-component navigation-component--mobile">
+				<div className="navigation-component__current-path">
+					{pathname}
+				</div>
+				<ButtonComponent
+					onClick={showMenuAction}
+					customCss={classNames('navigation-component__toggle-button', {
+						'navigation-component__toggle-button--active': showList,
+					})}>
+					<i className="fa-solid fa-bars"></i>
+				</ButtonComponent>
 
-			{
-				showList && (
-					<PopupDisplayComponent backgroundClick={hideMenuAction}>
-						<NavigationListComponent hideMenuAction={hideMenuAction} showList={showList} />
-					</PopupDisplayComponent>
-				)
-			}
-		</div>
+				{
+					showList && (
+						<PopupDisplayComponent backgroundClick={hideMenuAction}>
+							<NavigationListComponent hideMenuAction={hideMenuAction} showList={showList} />
+						</PopupDisplayComponent>
+					)
+				}
+			</div>
+
+			<div className="navigation-component navigation-component--desktop">
+				<NavigationListComponent hideMenuAction={hideMenuAction} showList={showList} />
+			</div>
+		</>
 	);
 };
 
