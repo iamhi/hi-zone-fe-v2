35d69e9c15c978edcc63175cb915921e1edac2c8
iamhi <inpactt@gmail.com>
2023-04-26 23:43:53 +0200

    Adding landing page

--- src/pages/SettingsRouteComponent.jsx
+++ src/pages/SettingsRouteComponent.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import ApplicationSelectComponent from '@components/ApplicationSelectComponent';
+import LandingPathSelectComponent from '@components/LandingPathSelectComponent';
 
 const SettingsRouteComponent = () => (
 	<div>
 		<ApplicationSelectComponent />
+
+		<LandingPathSelectComponent />
 	</div>
 );
 
